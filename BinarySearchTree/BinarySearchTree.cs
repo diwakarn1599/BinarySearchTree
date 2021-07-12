3fc5f8b1176d839e6bef6dd5d71179a0e3bd145b
Diwakar <diwakarn1599@gmail.com>
2021-07-12 21:09:55 +0530

    [Diwakar] Insert element into bst

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T:IComparable<T>
+    {
+        public T data { get; set; }
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+        public BinarySearchTree(T data)
+        {
+            this.data = data;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.data;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                {
+                    this.rightTree.Insert(item);
+                }
+            }
+        }
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.data.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,13 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Binary Search Tree");
+
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(100);
+
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Display();
         }
     }
 }
